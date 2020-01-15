733309ecad76422aae13551ab43400367e2a20a2
Next EngGen 1 <nextenggen1@Nexts-MacBook-Pro.local>
2020-01-15 16:10:37 +0200

    home page and header

---
+++ src/homePage/homePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homePage.css';
+
+export function HomePage() {
+  return (
+    <div className="HomePage">
+      <h2>OMG, it’s the best dating app EVER!</h2>
+    </div>
+  );
+}
